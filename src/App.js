d634a962726cf64f10f684bd06e11df96358e3dc
mm-e <magdaeaton@hotmail.com>
2022-08-16 18:26:48 -0300

    clase1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './oxno_gris.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This will be my OXNO e-commerce.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://mm-e.github.io/oxno/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about OXNO
         </a>
       </header>
     </div>
